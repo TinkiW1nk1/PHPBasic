ffe574f20094a5349bf6b79bf158ed6fea0e27c4
TinkiW1nk1 <reg.alex.potapov@gmail.com>
2022-12-11 16:24:31 +0200

    domashka6

---
+++ Domaska6/LenovoProduct.php
@@ -0,0 +1,27 @@
+<?php
+class LenovoProduct extends NotebookProduct
+{
+    public int $core;
+
+    public function setCore(int $core):void
+    {
+        $this->core = $core;
+    }
+    public function getCore(): int
+    {
+        return $this->core;
+    }
+    public function getCoreRam(): int
+    {
+        return $this->core - $this->price;
+    }
+    public function getAll(): int
+    {
+        return $this->coll + $this->core;
+    }
+
+/*    public function step(int $step): int
+    {
+        return pow($this->core, $step);
+    }*/
+}
